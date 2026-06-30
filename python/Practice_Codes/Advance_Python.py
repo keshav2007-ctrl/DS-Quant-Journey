a7b9a608fcf7a6359a9d7fb74011edaa6fa88719
Keshav Bhardwaj <bhardwajkeshav2007@gmail.com>
2026-06-30 17:32:27 +0530

    ..

--- python/Practice_Codes/Advance_Python.py
+++ python/Practice_Codes/Advance_Python.py
@@ -60,4 +60,4 @@ table = [str(7*i ) for i in range(1, 11)]
 s = "\n" .join(table)
 print(s)
 
- #map, enumerate, filter, reduce(this is imported from fuctools), virtual enviornment, format 
+ #map, enumerate, filter, reduce(this is imported from fuctools), virtual enviornment, format, flask
